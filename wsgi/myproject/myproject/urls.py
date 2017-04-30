ed33f36752c3127e6ebf61d3021c91a407300ba2
Benjamin Kopp <benjamin.kopp@scandio.de>
2017-04-30 17:27:58 +0200

    - add makemigrations hook - add url-route

--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from .views import EntryListView
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', EntryListView.as_view(), name="entry-list")
 ]
