1dedd20342e96265e3e68bf2764380526ff48e79
Benjamin Kopp <benjamin.kopp@scandio.de>
2017-05-01 00:36:07 +0200

    remove csrf for set_image

--- wsgi/myproject/myproject/views.py
+++ wsgi/myproject/myproject/views.py
@@ -2,6 +2,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404, redirect
+from django.views.decorators.csrf import csrf_exempt
 from .models import SkillTree
 
 
@@ -17,6 +18,7 @@ class SkillTreeDetailView(DetailView):
     template_name = "skilltree_detail.html"
 
 
+@csrf_exempt
 @require_POST
 def skilltree_setimage(request, skilltree_id):
     skilltree = get_object_or_404(SkillTree, id=skilltree_id)
