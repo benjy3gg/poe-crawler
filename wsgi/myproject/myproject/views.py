f718a16dde9a2058ac0b54acc257f1d8e41701ee
Benjamin Kopp <benjamin.kopp@scandio.de>
2017-04-30 17:49:45 +0200

    fixes

--- wsgi/myproject/myproject/views.py
+++ wsgi/myproject/myproject/views.py
@@ -6,7 +6,3 @@ class EntryListView(ListView):
 
     model = Entry
     template_name = "entry_list.html"
-
-    def get_queryset(self):
-        entries = Entry.object.all()
-        return entries
