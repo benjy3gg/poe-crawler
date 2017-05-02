bd62984dfd09d06c0ecd60400a338a47baf56974
benjy3gg <benjy3gg@googlemail.com>
2017-05-02 23:38:06 +0200

    fix fuckup

--- wsgi/myproject/myproject/models.py
+++ wsgi/myproject/myproject/models.py
@@ -36,7 +36,7 @@ class SkillTree(models.Model):
     level = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     image_url = models.CharField(max_length=20000)
-    image_file = models.ImageField(upload_to='images', sizes=((500,500), (1500,1500)))
+    image_file = models.ImageField(upload_to='images')
 
     class Meta:
         verbose_name_plural = "SkillTrees"
